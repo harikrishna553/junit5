a1be7477accdb7486f4ac95e52aa420c9940bb42
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2020-06-27 16:49:39 +0530

    Junit5 hello world maven example

---
+++ junit5-examples/src/test/java/com/sample/app/AppTest.java
@@ -0,0 +1,14 @@
+package com.sample.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+	@Test
+	public void helloTest() {
+		assertTrue(true);
+	}
+
+}
